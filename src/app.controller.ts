a24a1fbd691bb74c94de71a1045961bd0e1fa163
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-22 16:44:32 +0900

    feat: [main] Controller에서 query / param 데코레이터 사용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('get')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Get('paramName/:name')
+  getParamName(@Param('name')name: string): string {
+    return `${name} hello`;
+  }
+
+  @Get('queryName')
+  getQueryName(@Query('name')name: string): string {
+    return `${name} hello`;
   }
 }
