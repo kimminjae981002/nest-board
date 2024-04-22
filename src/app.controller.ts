306bb6e9ad8ed817412534ad6c77795f01c53f2c
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-22 21:59:20 +0900

    feat: [main] 기본 Logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, HttpException, HttpStatus, Param, Query } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Logger, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('get')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  private readonly logger = new Logger(AppController.name);
+
   @Get()
   getHello(): string {
-    throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
     return this.appService.getHello();
+  }
+
+  @Get('log')
+  log(): void {
+    this.logger.log('message')
+    this.logger.debug('message')
+    this.logger.error('message')
+    this.logger.fatal('message')
+    this.logger.verbose('message')
+    this.logger.warn('message')
   }
 
   @Get('exception')
