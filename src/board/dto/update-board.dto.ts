76e37dde6b493565b5a20e63d57523c4a78745a1
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-22 21:07:43 +0900

    feat: [main] dto type

--- src/board/dto/update-board.dto.ts
+++ src/board/dto/update-board.dto.ts
@@ -1,4 +1,6 @@
+import { PartialType, PickType } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, MaxLength, MinLength } from 'class-validator';
+import { CreateBoardDto } from './create-board.dto';
 
 export class UpdateBoardDto {
     @MaxLength(20)
@@ -9,3 +11,38 @@ export class UpdateBoardDto {
     @IsOptional()
     content: string;
 }
+
+
+// createboarddto에 상속한다. class-validator까지 상속받음
+// export class UpdateBoardDto extends PartialType(CreateBoardDto) {
+//     @MaxLength(20)
+//     @MinLength(2)
+//     @IsOptional()
+//     title: string;
+
+//     @IsOptional()
+//     content: string;
+// }
+
+
+// name필드만 받아온다.
+// export class UpdateBoardDto extends PickType(CreateBoardDto,['name']) {
+    //     @MaxLength(20)
+    //     @MinLength(2)
+    //     @IsOptional()
+    //     title: string;
+    
+    //     @IsOptional()
+    //     content: string;
+// }
+    
+// name필드만 제거하고 나머지를 가져온다.
+// export class UpdateBoardDto extends OmitType(CreateBoardDto,['name']) {
+    //     @MaxLength(20)
+    //     @MinLength(2)
+    //     @IsOptional()
+    //     title: string;
+    
+    //     @IsOptional()
+    //     content: string;
+    // }
