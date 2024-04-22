4724a69ddf4ded5025a97add427dc29a3b0205d2
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-22 21:10:17 +0900

    feat: [main] swagger ApiProperty

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { MaxLength, MinLength, IsNotEmpty, isNotEmpty } from 'class-validator';
 
 export class CreateBoardDto {
     
+    @ApiProperty({
+        description: '제목',
+        required: true,
+        example: '제목을 입력해주세요.'
+    })
     @IsNotEmpty()
     @MinLength(2)
     @MaxLength(20)
     title: string;
 
+    @ApiProperty({
+        description: '내용',
+        required: true,
+        example: '내용을 입력해주세요.'
+    })
     @IsNotEmpty()
     content: string;
 }
