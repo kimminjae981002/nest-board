406cd447b81545850fcd84c51df4d8b1a502e3b6
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-23 17:12:10 +0900

    feat: [main] entity swagger 설정

--- src/entity/board.entity.ts
+++ src/entity/board.entity.ts
@@ -1,14 +1,21 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity() // {name: simple_board} 테이블 명 수정 가능
 export class Board {
 
-    @PrimaryColumn({name: 'id'}) // 기본키이다.
+    @PrimaryGeneratedColumn({name: 'id'}) // 기본키이다. 자동적으로 숫자가 증가한다.
     id: number;
 
+    @ApiProperty({ description: 'user_id' }) // userid 외래키
+    @Column() // {name: simple_board} 컬럼 명 수정 가능
+    userId: number
+
+    @ApiProperty({description: '게시글 제목', example: '제목입니다.'})
     @Column() // 일반 컬럼
     title: string;
 
+    @ApiProperty({description: '게시글 내용', example: '내용입니다.'})
     @Column() // 일반 컬럼
     content: string;
 
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,18 +1,22 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 
 @Entity()
 export class User{
     
-    @PrimaryColumn()
+    @PrimaryGeneratedColumn() // 자동적으로 숫자가 증가한다.
     id: number;
 
+    @ApiProperty({description: '유저아이디', example: 'admin'})
     @Column({unique: true})
     username: string;
 
+    @ApiProperty({description: '유저비밀번호', example: 'password'})
     @Column({select:false})
     password: string;
 
+    @ApiProperty({description: '이름', example: '홍길동'})
     @Column()
     name: string;
 }
