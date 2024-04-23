a6a05d600635ca04ac45bc60ab23c8fca102e86e
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-23 16:58:07 +0900

    feat: [main] typeorm  적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { BoardController } from './board/board.controller';
 import { BoardModule } from './board/board.module';
 import { LoggingMiddleware } from './middlewares/logging.middleware';
 import  ConfigModule  from './config/index';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres', // database
+      host: process.env.DB_HOST, // db host
+      port: +process.env.DB_PORT, // db port
+      username: process.env.DB_USERNAME, // db username
+      password: process.env.DB_PW, // db user pw
+      database: 'postgres', // db 이름
+      entities: [__dirname + '/**/*.entity.{.ts,.js}'], // entity 파일 경로
+      synchronize: false, // entity 파일 값이 변경되었을 때 db에 반영하겠나?
+    }),
     BoardModule],
   controllers: [AppController],
   providers: [AppService],
